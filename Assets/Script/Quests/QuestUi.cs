dc913f4871f3f89617baba5bc8aa7f079f07e98d
Lukhern <90791937+Luriko@users.noreply.github.com>
2025-03-29 12:00:44 +0200

    fixed QuestUi Quest list is now appearing

--- Assets/Script/Quests/QuestUi.cs
+++ Assets/Script/Quests/QuestUi.cs
@@ -7,23 +7,20 @@ public class QuestUI : MonoBehaviour
     public TMP_Text questListText;
     private QuestManager questManager;
     private Find find;
-
-    void Start()
-    {
-        questManager = find.FindUIElement<QuestManager>("QuestManager");
-        // UpdateQuestList();
-    }
-
     public void UpdateQuestList()
     {
         questListText.text = "Задания:\n";
-        if (questManager != null)
+
+        if (questManager == null)
         {
-            foreach (Quest quest in questManager.GetActiveQuests())
-            {
-                questListText.text += "- " + quest.title + ": " + quest.description + "\n";
-            }
-            
+            //Костыль
+            questManager = GameObject.Find("QuestManager").GetComponent<QuestManager>();
+        }
+        else;
+
+        foreach (var quest in questManager.GetActiveQuests())
+        {
+            questListText.text += "- " + quest.title + ": " + quest.description + "\n";
         }
     }
 }
